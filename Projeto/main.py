2aacfa0f3177f7e9c1c584777392ad08bc087cf7
ZhouZKZ <roberto16092004@gmail.com>
2023-11-30 22:25:19 -0300

    Criando um DataFrame da Carteira com as quantidades de ações

--- Projeto/main.py
+++ Projeto/main.py
@@ -12,16 +12,16 @@ for ativo in carteira['Ativos']:
     cotacoes_carteira[ativo] = pdr.DataReader('{}.SA'.format(ativo), start='2020-01-01', end='2020-11-10')['Adj Close']
 
 
-#       Ajustando os dados(Tratando Problemas de Importação pro DataFrame e Normalização)
+#   Ajustando os dados(Tratando Problemas de Importação pro DataFrame e Normalização)
 
+cotacoes_carteira.info()
 #  Preencher com a média da coluna
 """df_media = cotacoes_carteira.mean()
 cotacoes_carteira = cotacoes_carteira.fillna(df_media)"""
-
 #  Preencher com o próximo valor não vazio da sequência
 cotacoes_carteira = cotacoes_carteira.bfill()
-
 cotacoes_carteira.info()
+
 
 #   Como Que As Ações Foram Individualmente
 carteira_nom = cotacoes_carteira / cotacoes_carteira.iloc[0]
@@ -29,3 +29,13 @@ carteira_nom = cotacoes_carteira / cotacoes_carteira.iloc[0]
 carteira_nom.plot(figsize=(15, 5))
 plt.legend(loc='upper left')
 plt.show()
+
+
+#   Criando um DataFrame da Carteira com as quantidades de ações
+valor_investido = pd.DataFrame()
+for ativo in carteira['Ativos']:
+    valor_investido[ativo] = cotacoes_carteira[ativo] * carteira.loc[carteira['Ativos']==ativo, 'Qtde'].values[0]
+#print(valor_investido)
+
+valor_investido['Total'] = valor_investido.sum(axis=1)
+print(valor_investido.iloc[0])
